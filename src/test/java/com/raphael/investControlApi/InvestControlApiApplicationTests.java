88a3d576b7a20e759173623e04dafb160d2f70e8
rsaniceto14 <raphaelaniceto14@gmail.com>
2025-05-14 16:25:38 -0300

    [chore] first commit - env config

---
+++ src/test/java/com/raphael/investControlApi/InvestControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.investControlApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
